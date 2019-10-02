0b18feeb8a7768090383cc6f2a5024235753b577
Allan Ricardo Pasquali <allan_pasquali@hotmail.com>
2019-10-02 11:20:48 -0300

    Banco dados HBSIS 2

---
+++ BancoDeDadosHbsis 2/SQLFiltros.sql
@@ -0,0 +1,13 @@
+﻿select * from Tipos
+select * from Marcas
+select * from ModelosVeiculos 
+select * from ModelosVeiculos vei inner join Marcas mar on vei.MarcaCodigo =  mar.Codigo
+								  inner join Tipos tip on mar.TipoCodigo = tip.Codigo
+select vei.Id, vei.Modelo, mar.Nome as 'Marca',tip.Nome as 'Tipo' from ModelosVeiculos vei inner join Marcas mar on vei.MarcaCodigo =  mar.Codigo
+																                    		inner join Tipos tip on mar.TipoCodigo = tip.Codigo
+select vei.Id, vei.Modelo, mar.Nome as 'Marca',tip.Nome as 'Tipo' from ModelosVeiculos vei inner join Marcas mar on vei.MarcaCodigo =  mar.Codigo
+																                    		inner join Tipos tip on mar.TipoCodigo = tip.Codigo
+																							where mar.Codigo = 20 -- Moto Kawasaki
+select vei.Id, vei.Modelo, mar.Nome as 'Marca',tip.Nome as 'Tipo' from ModelosVeiculos vei inner join Marcas mar on vei.MarcaCodigo =  mar.Codigo
+																                    		inner join Tipos tip on mar.TipoCodigo = tip.Codigo
+																							where mar.Codigo = 10 -- Carro Kia
